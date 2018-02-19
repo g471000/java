2d7e46370b27fca9e8dd54aef7a9b7f5595f3b41
Jieun Chon <g471000@vt.edu>
2018-02-19 04:11:26 -0500

    helloworld done

---
+++ Introduction/src/_01hello/Solution.java
@@ -0,0 +1,20 @@
+package _01hello;
+
+/**
+ * 
+ * Hello World
+ * 
+ * @author Jieun Chon
+ * @version Feb 19, 2018 4:08:13 AM
+ *
+ */
+public class Solution {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+		System.out.println("I am Jieun Chon!");
+	}
+}
