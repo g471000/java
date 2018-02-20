0d348fa626c18f17bd8f9c0f740d71fcfff97ce4
Jieun Chon <g471000@vt.edu>
2018-02-20 00:21:09 -0500

    in and out 2 done

--- Introduction/src/introduction/_03IfElse.java
+++ Introduction/src/introduction/_03IfElse.java
@@ -12,28 +12,32 @@ import java.util.*;
 public class _03IfElse {
 
 	/**
-	 * 1 <= n <= 100 
+	 * 1 <= n <= 100
 	 * 
-	 * Task Given an integer, , perform the following conditional
-	 * actions:
+	 * Task Given an integer, , perform the following conditional actions:
 	 * 
-	 * If is odd, print Weird If is even and in the inclusive range of to ,
-	 * print Not Weird If is even and in the inclusive range of to , print Weird
-	 * If is even and greater than , print Not Weird
+	 * If is odd, print Weird
+	 * 
+	 * If is even and in the inclusive range of 6 to 20, print Weird. Otherwise,
+	 * print Not Weird
 	 * 
 	 * @param args
 	 *            argument
 	 */
 	public static void main(String[] args) {
 
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		String ans = "";
 		if (n % 2 == 1) {
 			ans = "Weird";
 		} else {
-
-
+			if (n >= 6 && n <= 20) {
+				ans = "Weird";
+			} else {
+				ans = "Not Weird";
+			}
 		}
 		System.out.println(ans);
 
